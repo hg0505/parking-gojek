bf8ba58a2c39ee68286d44501ee64ed0181a05f5
harshitgarg1 <harshit.garg1@oyorooms.com>
2019-06-16 18:30:45 +0530

    initial commit and addition of models

---
+++ src/main/java/com/parking/model/ParkingSlot.java
@@ -0,0 +1,51 @@
+package com.parking.model;
+
+public class ParkingSlot {
+
+    int slotNumber;
+
+    boolean isOccupied = false;
+
+    Vehicle vehicle;
+
+
+
+    public int getSlotNumber() {
+        return slotNumber;
+    }
+
+    public void setSlotNumber(int slotNumber) {
+        this.slotNumber = slotNumber;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean isOccupied) {
+        this.isOccupied = isOccupied;
+    }
+
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public ParkingSlot(int slotNumber, int level, boolean isOccupied) {
+        super();
+        this.slotNumber = slotNumber;
+        this.isOccupied = isOccupied;
+    }
+
+    public ParkingSlot(int slotNumber, int level, boolean isOccupied, Vehicle vehicle) {
+        super();
+        this.slotNumber = slotNumber;
+        this.isOccupied = isOccupied;
+        this.vehicle = vehicle;
+    }
+
+}
---
+++ src/main/java/com/parking/model/Vehicle.java
@@ -0,0 +1,31 @@
+package com.parking.model;
+
+public class Vehicle {
+
+    private String registrationNumber;
+    private String color;
+
+    public Vehicle(String registrationNumber, String color) {
+        super();
+        this.registrationNumber = registrationNumber;
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+}
